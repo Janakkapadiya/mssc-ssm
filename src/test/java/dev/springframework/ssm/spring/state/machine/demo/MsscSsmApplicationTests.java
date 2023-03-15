3ede7605e28ceb5ea1c4128a101b6b4298bd7b52
Janak <jaykapadiya8899@gmail.com>
2023-03-15 16:51:59 +0530

    initial commit

---
+++ src/test/java/dev/springframework/ssm/spring/state/machine/demo/MsscSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.springframework.ssm.spring.state.machine.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscSsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
